acdd1c7eee0c4860c903f5c462e442b94083f896
Jakey-X <59095266+Jakey-X@users.noreply.github.com>
2022-12-14 11:21:35 +0800

    Add some code comments

--- HelloWorld.go
+++ HelloWorld.go
@@ -12,18 +12,25 @@ import (
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
+// Functions to refresh metric values at regular intervals
 func recordMetrics() {
+	// Open concordance(?or I should say goroutines)
 	go func() {
 		for {
-			cc, _ := cpu.Percent(time.Second, false)
+			// Use cc to accept the CPU utilization returned by the function
+			cc, _ := cpu.Percent(time.Second, false) //get CPU utilization based on 1 second
 			opsProcessed.Set(cc[0])
+			// Refresh every two seconds
 			time.Sleep(2 * time.Second)
 		}
 	}()
 }
 
 var (
+	// Define a metric of type gauge as a global variable.
+	// Many of the New* functions under the promauto package are useful for building and automatically registering various types of metrics
 	opsProcessed = promauto.NewGauge(prometheus.GaugeOpts{
+		// Options passed to the gauge builder
 		Name:        "just_a_test",
 		Help:        "Nothing is certain",
 		ConstLabels: prometheus.Labels{"myid": "jakey"},
@@ -31,8 +38,10 @@ var (
 )
 
 func main() {
+	// Execute the function and open the concurrent process to refresh the value of the metric every 2 seconds
 	recordMetrics()
 
+	// Expose the "/metrics" path to port 8080 and use promhttp to handle http requests, thus implementing exporter
 	http.Handle("/metrics", promhttp.Handler())
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
